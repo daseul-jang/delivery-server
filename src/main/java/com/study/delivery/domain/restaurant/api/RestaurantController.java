87af1b7396fe31cac7421c9eee266fd943beaf0c
daseul-jang <devzzang94@gmail.com>
2024-06-12 21:39:55 +0900

    refactor: 오프셋 방식의 페이지네이션 적용

--- src/main/java/com/study/delivery/domain/restaurant/api/RestaurantController.java
+++ src/main/java/com/study/delivery/domain/restaurant/api/RestaurantController.java
@@ -3,13 +3,13 @@ package com.study.delivery.domain.restaurant.api;
 import com.study.delivery.domain.order.order.dto.response.OrderResponse;
 import com.study.delivery.domain.restaurant.application.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +18,11 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @GetMapping("{id}/orders")
-    public ResponseEntity<List<OrderResponse>> getOrder(@PathVariable("id") Long restaurantId) {
-        return ResponseEntity.ok(restaurantService.getRestaurantOrders(restaurantId));
+    public ResponseEntity<Page<OrderResponse>> getOrders(
+            @PathVariable("id") Long restaurantId,
+            @RequestParam(name = "start", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+            @RequestParam(name = "end", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end,
+            Pageable pageable) {
+        return ResponseEntity.ok(restaurantService.getRestaurantOrders(restaurantId, start, end, pageable));
     }
 }
--- src/main/java/com/study/delivery/domain/restaurant/application/RestaurantServiceImpl.java
+++ src/main/java/com/study/delivery/domain/restaurant/application/RestaurantServiceImpl.java
@@ -4,17 +4,18 @@ import com.study.delivery.domain.order.order.dao.OrderMenuRepository;
 import com.study.delivery.domain.order.order.dao.OrderRepository;
 import com.study.delivery.domain.order.order.dto.response.OrderMenuResponse;
 import com.study.delivery.domain.order.order.dto.response.OrderResponse;
-import com.study.delivery.domain.order.order.entity.OrderMenu;
+import com.study.delivery.domain.order.order.entity.Order;
 import com.study.delivery.domain.restaurant.dao.RestaurantRepository;
 import com.study.delivery.domain.restaurant.entity.Restaurant;
 import com.study.delivery.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -25,17 +26,20 @@ public class RestaurantServiceImpl implements RestaurantService {
     private final OrderMenuRepository orderMenuRepository;
 
     @Override
-    public List<OrderResponse> getRestaurantOrders(Long restaurantId) {
+    public Page<OrderResponse> getRestaurantOrders(Long restaurantId,
+                                                   LocalDate start,
+                                                   LocalDate end,
+                                                   Pageable pageable) {
         Restaurant restaurant = restaurantRepository.findById(restaurantId)
                 .orElseThrow(() -> new BusinessException(HttpStatus.NOT_FOUND, "존재하지 않는 가게입니다."));
 
-        return orderRepository.findAllByRestaurant(restaurant).stream()
-                .map(order -> {
-                    List<OrderMenu> orderMenus = orderMenuRepository.findAllByOrder(order);
-                    List<OrderMenuResponse> orderMenuResponses =
-                            orderMenus.stream().map(OrderMenuResponse::of).toList();
+        Page<Order> pageOrders = orderRepository.findAllByRestaurantAndCreatedAtBetween(restaurant,
+                start.atStartOfDay(),
+                end.atTime(23, 59, 59),
+                pageable);
 
-                    return OrderResponse.of(order, orderMenuResponses);
-                }).collect(Collectors.toList());
+        return pageOrders.map(order ->
+                OrderResponse.of(order, order.getOrderMenus().stream().map(OrderMenuResponse::of).toList())
+        );
     }
 }
